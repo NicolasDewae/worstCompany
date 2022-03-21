2549b1e48ba644319cdd727f19ac046ebccc60c8
De waegenaere <de_waegenaere@live.fr>
2022-03-21 15:28:44 +0100

    Create worstTab with name, grade, url

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const puppeteer = require('puppeteer');
 
+const research = "coiffeur lille";
+const gradeMax = 2;
+
 (async () => {
     
     const browser = await puppeteer.launch(
@@ -27,8 +30,6 @@ const puppeteer = require('puppeteer');
 
     // send research
     try {
-      // Create research
-      const research = "coiffeur lille";
       // find input research by class
       const searchInput = await page.$("#searchbox");
       // insert keyword 
@@ -63,43 +64,58 @@ const puppeteer = require('puppeteer');
       });
     }
 
+    // click on "next page" button
+    async function goToNextPage(page) {
+      await page.click('button[aria-label="Page suivante"]');
+      await page.waitForNetworkIdle();
+    }
 
+    // search if is the last page
+    async function hasNextPage(page) {
+      const element = await page.$('button[aria-label="Page suivante"]');
+      if (!element) {
+        throw new Error('La page suivante n\'a pas était trouvée');
+      }      
+      const disabled = await page.evaluate((el) => el.getAttribute('disabled'), element);
+      if (disabled) {
+        console.log('Recherche terminée');
+      }
+      return !disabled;
+    }
+
+    // Find companies and create tab 
     async function parseCompany(page) {
-      let worstTab = [];
-      let bestTab = [];
-      
+      let worstTab = [];      
       const grades = await page.$$('span.ZkP5Je span.MW4etd');
-      
+      // Create tab
       if (grades && grades.length) {
         for(const el of grades){
           const grade = await el.evaluate(span => span.innerHTML);
-          const name = await el.evaluate(span => span.offsetParent.__cdn.Df.element.ariaLabel)
-          // console.log(url);
-          if (grade[0] <= 2) {
+          const name = await el.evaluate(span => span.offsetParent.__cdn.Df.element.ariaLabel);
+          const url = await el.evaluate(span => span.offsetParent.__cdn.context.H.context[6]);
+          // [0].offsetParent.__cdn.context.H.context[6]
+          if (grade[0] <= gradeMax) {
             worstTab.push({ 
               name: name,
-              grade: grade 
+              grade: grade,
+              url: url
             });
-          } else {
-            bestTab.push({ 
-              name: name,
-              grade: grade
-             });  
           }
         }
       }  
-
-      return {
-        worstTab,
-        bestTab
-      }
-      
+      return worstTab
     }
 
-    await autoScroll(page);
-
-    const company = await parseCompany(page);
-    console.log(company);
+    // Execute functions
+    let companies = [];
+    do {
+      await autoScroll(page);
+      companies = companies.concat(await parseCompany(page));
+      console.log('Recherche en cours');
+      await goToNextPage(page);  
+    } while (await hasNextPage(page));
+    
+    console.log(companies);
 
     await browser.close();
 
