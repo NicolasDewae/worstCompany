3c60c583d0828704f78490252f7472c46a8058ea
De waegenaere <de_waegenaere@live.fr>
2022-03-21 10:01:53 +0100

    Tab with name and grades

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // insérer la recherche à executer dans google maps
-    const research = "coiffeur lille";
     
     const browser = await puppeteer.launch(
       {
@@ -20,12 +18,17 @@ const puppeteer = require('puppeteer');
     const [AcceptCookies] = await page.$x("//span[contains(., 'accepte')]");
     if (AcceptCookies) {
         await AcceptCookies.click();
+        await page.waitForSelector("#searchbox");
         console.log("page de cookies accepée");
     } else {
       console.log("Il n'y a pas eu la page de cookies ");
+      await page.waitForSelector("#searchbox");
     }
 
+    // send research
     try {
+      // Create research
+      const research = "coiffeur lille";
       // find input research by class
       const searchInput = await page.$("#searchbox");
       // insert keyword 
@@ -37,28 +40,31 @@ const puppeteer = require('puppeteer');
       console.log("une erreur c'est produite au moment de lancer la recherche, pour plus de détail " + error)      
     }
 
-
-
-
     await page.waitForSelector("a.a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd");
 
 
-    async function parseTitle(page) {
-      let titlesTab = [];
-      
-      const titles = await page.$$('.gm2-subtitle-alt-1 span');
-      
-      if (titles && titles.length) {
-        for(const el of titles){
-          const title = await el.evaluate(span => span.innerText);
-          titlesTab.push({ title });
-        }
-      }
-
-      return titlesTab;
+    async function autoScroll(page){
+      await page.evaluate(async () => {
+          await new Promise((resolve, reject) => {
+              var totalHeight = 0;
+              var distance = 100;
+              var timer = setInterval(() => {
+                  const element = document.querySelectorAll('.section-scrollbox')[1];
+                  var scrollHeight = element.scrollHeight;
+                  totalHeight += distance;
+  
+                  if(totalHeight >= scrollHeight){
+                      clearInterval(timer);
+                      resolve();
+                  }
+              }, 100);
+          });
+      });
     }
 
+
     async function parseStars(page) {
+      let worstTab = [];
       let starsTab = [];
       
       const stars = await page.$$('span.ZkP5Je span.MW4etd');
@@ -66,33 +72,33 @@ const puppeteer = require('puppeteer');
       if (stars && stars.length) {
         for(const el of stars){
           const star = await el.evaluate(span => span.innerHTML);
-          starsTab.push({ star });
+          const name = await el.evaluate(span => span.offsetParent.__cdn.Df.element.ariaLabel)
+          // console.log(url);
+          if (star[0] <= 2) {
+            worstTab.push({ 
+              name: name,
+              star: star 
+            });
+          } else {
+            starsTab.push({ 
+              name: name,
+              star: star
+             });  
+          }
         }
       }  
 
-      return starsTab;
+      return {
+        worstTab,
+        starsTab
+      }
+      
     }
 
+    await autoScroll(page);
 
-    const titles = await parseTitle(page);
+    const titles = await parseStars(page);
     console.log(titles);
-    const stars = await parseStars(page);
-    console.log(stars);
-
-    // console.log('before waiting');
-    // await page.evaluate(async() => {
-    //   await new Promise(function(resolve) { 
-    //          setTimeout(resolve, 10000);
-    //   });
-    // });
-    // console.log('after waiting');
-
-
-    //  // Mettre les éléments dans le tableau
-    //  keyWord.push({
-    //    title: element.querySelector("h1"),
-    //    phone: element.querySelector("div.QSFF4-text gm2-body-2")
-    //  });
 
     await browser.close();
 
