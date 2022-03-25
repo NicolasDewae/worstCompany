bfe83c37e6971130c10e1897f48080143e8f3079
De waegenaere <de_waegenaere@live.fr>
2022-03-25 15:07:00 +0100

    index.js cleaned

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
 compFunctionExports = require('./company_function');
 
-const myArgs = process.argv.slice(2);
 
 (async () => {
     
@@ -17,68 +16,25 @@ const myArgs = process.argv.slice(2);
     await page.setViewport({ width: 1680, height: 700 });
     await page.goto('https://maps.google.fr', {waitUntil: 'networkidle2'});
 
-    // skip consent page if exist
-    try {
-      const [AcceptCookies] = await page.$x("//span[contains(., 'accepte')]");
-      if (AcceptCookies) {
-          await AcceptCookies.click();
-          await page.waitForSelector("#searchbox");
-          console.log("page de cookies accepée");
-      } else {
-        console.log("Il n'y a pas eu la page de cookies ");
-        await page.waitForSelector("#searchbox");
-      }
-    } catch (error) {
-      console.log("Une erreur c'est produite");      
-      console.log("Pour plus de détails: " + error);
-    }
 
-    // send research
-    try {
-      // find input research by class
-      const searchInput = await page.$("#searchbox");
-      // insert keyword 
-      for (let index = 0; index < myArgs.length; index++) {
-        await searchInput.type(myArgs[index] + " "); 
-      }
-      // send research
-      await page.click("#searchbox-searchbutton");
-      console.log("La recherche est lancée");      
-    } catch (error) {
-      console.log("Une erreur c'est produite au moment de lancer la recherche");      
-      console.log("Pour plus de détails: " + error);
-    }
+    // Execute functions
+    await compFunctionExports.consentPage(page);
+    await compFunctionExports.sendResearch(page);
 
     await page.waitForSelector("a.a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd");
 
-    // Execute functions
     let companies = [];
+
     do {
       await compFunctionExports.autoScroll(page);
       companies = companies.concat(await compFunctionExports.parseCompany(page));
       console.log('Recherche en cours, nombre d\'entreprises trouvées actuellement ' + companies.length);
       await compFunctionExports.goToNextPage(page);  
     } while (await compFunctionExports.hasNextPage(page));
+
+    await compFunctionExports.sortByWorst(companies);
     
-    // Sort by worst grade and return result
-    try {
-      companies.sort(function compare(a, b) {
-        if (a.grade < b.grade) {
-          return -1;          
-        }
-        if (a.grade > b.grade) {
-          return 1;         
-        }
-        return 0;
-      });
-      // Final result
-      console.table(companies);
-    } catch (error) {
-      console.log("Une erreur c'est produite au moment de traiter le tableau");
-      console.log("Pour plus de détails: " + error);
-    }
+    console.table(companies);
 
     await browser.close();
-
-
 })();
