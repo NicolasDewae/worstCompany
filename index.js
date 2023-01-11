205ac0c521096a13d3b063a40a475707ed6aa9f7
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2023-01-11 14:32:47 +0100

    process script updated

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
 const puppeteer = require('puppeteer');  
 compFunctionExports = require('./company_function');
 
-
 (async () => {
     
-    const browser = await puppeteer.launch(
-      {
-        userDataDir: ("./user_data"),
-        headless: true,
-      }
+  const browser = await puppeteer.launch(
+    {
+      userDataDir: ("./user_data"),
+      headless: true,
+    }
   );
     
-    const page = await browser.newPage();
-    // Full screen
-    await page.setViewport({ width: 1680, height: 700 });
-    await page.goto('https://maps.google.fr', {waitUntil: 'networkidle2'});
+  const page = await browser.newPage();
+  // Screen size
+  await page.setViewport({ width: 1680, height: 700 });
+  await page.goto('https://maps.google.fr', {waitUntil: 'networkidle2'});
 
+  // Execute functions
+  await compFunctionExports.consentPage(page);
+  await compFunctionExports.sendResearch(page);
 
-    // Execute functions
-    await compFunctionExports.consentPage(page);
-    await compFunctionExports.sendResearch(page);
+  await page.waitForSelector("div.bfdHYd");
 
-    await page.waitForSelector("a.a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd");
+  let companies = [];
+  
+  // Scroll to the bottom
+  await compFunctionExports.autoScroll(page, 'div.bfdHYd');
+  
+  companies = companies.concat(await compFunctionExports.parseCompany(page));
 
-    let companies = [];
-    
-    // While there is new page, continue
-    do {
-      await compFunctionExports.autoScroll(page);
-      companies = companies.concat(await compFunctionExports.parseCompany(page));
-      console.log('Recherche en cours, nombre d\'entreprises trouvées actuellement ' + companies.length);
-      await compFunctionExports.goToNextPage(page);  
-    } while (await compFunctionExports.hasNextPage(page));
-
-    // Sort grade by descending order
-    await compFunctionExports.sortByWorst(companies);
-    
-    // Show result
-    console.log(companies);
-
-    await browser.close();
+  console.log("parseCompany terminé");
+  // Sort grade by descending order
+  await compFunctionExports.sortByWorst(companies);
+  
+  // Show result
+  console.log(companies);
+  await browser.close();
 })();
