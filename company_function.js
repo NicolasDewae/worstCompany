1bee5339135e8aae4237fcca301a57ae73f51f20
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2023-01-11 14:32:03 +0100

    isLastCompany function added + autoScroll updated + parseCompany updated

--- company_function.js
+++ company_function.js
@@ -34,98 +34,120 @@ exports.sendResearch = async function sendResearch(page) {
 }
 
 /**
+ * @param {*} page 
+ * @returns true if is the last company, false if not
+ */
+async function lastCompany(page) {
+  if (await page.$(".HlvSq") == null) {
+    console.log('Scroll en cours...');
+    return false;
+  } 
+  else {
+    console.log('Scroll terminé');
+    return true;
+  }
+}
+
+/**
  * Scroll at the bottom
  * @param {*} page 
  */
-exports.autoScroll = async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 300;
-            var timer = setInterval(() => {
-                var element = document.querySelectorAll('.section-scrollbox')[1];
-                var scrollHeight = element.scrollHeight;
-                element.scrollBy(0, distance);
-                totalHeight += distance;
-
-                if(totalHeight >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
-    });
-  }
-
-  /**
-   * Click on "next page" button
-   * @param {*} page 
-   */
-  exports.goToNextPage = async function goToNextPage(page) {
-    await page.click('button[aria-label="Page suivante"]');
-    await page.waitForNetworkIdle();
-  }
-
-  /**
-   * Search if is the last page
-   * @param {*} page 
-   * @returns !disabled
-   */
-  exports.hasNextPage = async function hasNextPage(page) {
-    const element = await page.$('button[aria-label="Page suivante"]');
-    if (!element) {
-      throw new Error('La page suivante n\'a pas était trouvée');
-    }      
-    const disabled = await page.evaluate((el) => el.getAttribute('disabled'), element);
-    if (disabled) {
-      console.log('Recherche terminée');
+exports.autoScroll = async function scrollPage(page, scrollElements) {
+  let currentElement = 0;
+  while (true) {
+    let elementsLength = await page.evaluate((scrollElements) => {
+      return document.querySelectorAll(scrollElements).length;
+    }, scrollElements);
+    for (; currentElement < elementsLength; currentElement++) {
+      await page.waitForTimeout(200);
+      await page.evaluate(
+        (currentElement, scrollElements) => {
+          document.querySelectorAll(scrollElements)[currentElement].scrollIntoView();
+        },
+        currentElement,
+        scrollElements
+      );
     }
-    return !disabled;
+    await page.waitForTimeout(5000);
+    let newElementsLength = await page.evaluate((scrollElements) => {
+      return document.querySelectorAll(scrollElements).length;
+    }, scrollElements);
+    if (newElementsLength === elementsLength) break;
+    if (lastCompany(page) == true) break;
   }
+}
 
-  /**
-   * Find companies and create tab
-   * @param {*} page 
-   * @returns tab with companies
-   */
-  exports.parseCompany = async function parseCompany(page) {
-    let worstTab = [];      
-    const grades = await page.$$('span.ZkP5Je span.MW4etd');
-    // Create tab
-    if (grades && grades.length) {
-      for(const el of grades){
-        const name = await el.evaluate(span => span.offsetParent.__cdn.Df.element.ariaLabel);
-        const grade = await el.evaluate(span => span.innerHTML);
-        const nbComm = await el.evaluate(span => span.parentElement.lastChild.innerText);
-        // const adress = await el.evaluate(span => span.parentElement.parentElement.parentElement.parentNode.parentNode.lastElementChild.children[1].outerText);
-        const url = await el.evaluate(span => span.offsetParent.__cdn.context.H.context[6]);
-        worstTab.push({ 
-            name: name,
-            grade: grade,
-            nbComm: nbComm,
-            // adress: adress,
-            url: url
-        });
-      }
-    }  
-    return worstTab
+/**
+ * Click on "next page" button
+ * @param {*} page 
+ */
+exports.goToNextPage = async function goToNextPage(page) {
+  await page.click('button[aria-label="Page suivante"]');
+  await page.waitForNetworkIdle();
+}
+
+/**
+ * Search if is the last page
+ * @param {*} page 
+ * @returns !disabled
+ */
+exports.hasNextPage = async function hasNextPage(page) {
+  const element = await page.$('button[aria-label="Page suivante"]');
+  if (!element) {
+    throw new Error('La page suivante n\'a pas était trouvée');
+  }      
+  const disabled = await page.evaluate((el) => el.getAttribute('disabled'), element);
+  if (disabled) {
+    console.log('Recherche terminée');
   }
+  return !disabled;
+}
 
-  /**
-   * Sort by worst grade
-   * @param {*} companies 
-   * @returns final tab
-   */
-  exports.sortByWorst = async function sortByWorst(companies) {
-      companies.sort(function compare(a, b) {
-        if (a.grade < b.grade) {
-          return -1;          
-        }
-        if (a.grade > b.grade) {
-          return 1;         
-        }
-        return 0;
+/**
+ * Find companies and create tab
+ * @param {*} page 
+ * @returns tab with companies
+ */
+exports.parseCompany = async function parseCompany(page) {
+  let companies = [];      
+  const lists = await page.$$('span.ZkP5Je');
+  console.log("Nombre d'entreprises trouvées : " + lists.length);
+  // Create tab
+  if (lists && lists.length) {
+    for(const el of lists){
+      const name =    await el.evaluate(span => span.parentElement.parentElement.parentElement.parentElement.children[0].innerText);
+      const grade =   await el.evaluate(span => span.children[0].innerHTML);
+      const nbComm =  await el.evaluate(span => span.children[7].innerHTML);
+      const adress =  await el.evaluate(span => span.parentElement.parentElement.parentElement.parentElement.parentElement.children[1].children[3].children[1].innerText);
+      const url =     await el.evaluate(span => span.baseURI);
+      companies.push({ 
+          name: name,
+          grade: grade,
+          nbComm: nbComm,
+          adress: adress,
+          url: url
       });
-      // Final result
-      return companies; 
-  }
+    }
+  }  
+  return companies;
+}
+
+/**
+ * Sort by worst grade
+ * @param {*} companies 
+ * @returns final tab
+ */
+exports.sortByWorst = async function sortByWorst(companies) {
+    console.log("Tri des entreprises par note");
+    companies.sort(function compare(a, b) {
+      if (a.grade < b.grade) {
+        return -1;          
+      }
+      if (a.grade > b.grade) {
+        return 1;         
+      }
+      return 0;
+    });
+    // Final result
+    return companies; 
+}
